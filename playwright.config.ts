75078f67ffeb1236e16badfffc76e2f8375a9b60
Talha Malik <169103889+MrTalhaSami@users.noreply.github.com>
2026-07-22 04:42:18 +0500

    Use headed

--- playwright.config.ts
+++ playwright.config.ts
@@ -28,6 +28,7 @@ export default defineConfig({
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
     baseURL: 'https://portfolio-drab-five-50.vercel.app/',
+    headless:true,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -37,7 +38,7 @@ export default defineConfig({
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'] , headless:true},
+      use: { ...devices['Desktop Chrome'] },
     },
 
    
